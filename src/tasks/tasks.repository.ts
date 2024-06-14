ee4f80709a97018d319aec1955744d38eb2962b8
Jonson Ncube <jonson.ncube@ct-mac-12.home>
2024-06-14 12:47:12 +0200

    added repositoryDDDDD

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,4 +1,5 @@
-import { Repository } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import { TaskEntity } from './task.entity';
 
-export class TaskRepository extends Repository<TaskEntity> {}
+@EntityRepository(TaskEntity)
+export class TasksRepository extends Repository<TaskEntity> {}
